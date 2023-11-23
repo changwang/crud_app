1bd8f9e44cb5682912eec7ce2ae8046542329ff5
Chang Wang <changwang83@gmail.com>
2023-11-23 12:37:08 -0800

    Add handlers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use axum::http;
+mod handlers;
 use axum::routing::{get, Router};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let port = std::env.var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let addr = format!("0.0.0.0:{}", port);
 
-    let app = Router::new().route("/", get(health));
+    let app = Router::new().route("/", get(handlers::health));
 
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
@@ -14,8 +14,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
     Ok(())
-}
-
-async fn health() -> http::StatusCode {
-    http::StatusCode::OK
 }
